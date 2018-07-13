a0a65dc96ed5866735f3f842f4664398db551b56
jessicamh13 <jessica.mh13@yahoo.com>
2018-07-13 14:15:13 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,5 +1,16 @@
 #code your solutions here
-
+ perfect_10_ingredients = {
+      :almond_flour => "2 cups",
+      :gluten_freewhole_oats => "1 cup",
+      :kosher_salt => "1/2 tea spoon",
+      :baking_powder  => "1/2 tea spoon",
+      :baking_soda => "1/4 tea spoon",
+      :xanthan_Gum => "1/2 tea spoon",
+      :slivered_almonds => "1/4 cup",
+      :mini_dark_chocolate_chips => "3/4 cup",
+      :olive_oil => "1/2 cup",
+      :agave => "1/4 cup"
+    }
 
   puts perfect_10_ingredients
 
